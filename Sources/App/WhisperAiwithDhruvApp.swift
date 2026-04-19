b0010175d3f26ee773464f3f4fe58ffd544ae283
Dhruv Tomar <aiwithdhruv@gmail.com>
2026-04-20 03:29:46 +0530

    feat(cloud): wire SupabaseService with Info.plist keys (IW-005)

--- Sources/App/WhisperAiwithDhruvApp.swift
+++ Sources/App/WhisperAiwithDhruvApp.swift
@@ -22,6 +22,7 @@ struct IndianWhisperApp: App {
     init() {
         NSApplication.shared.setActivationPolicy(.accessory)
         logToFile("App init called")
+        _ = SupabaseService.shared
         Task.detached {
             do {
                 logToFile("Starting setup...")
